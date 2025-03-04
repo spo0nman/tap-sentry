93d37a69fdc828b92993ddde3a8d8d1128a44d58
Pankaj Kaushal <pankaj@getcalmo.com>
2025-03-04 16:13:06 +0100

    Bump package version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
       name="tap-sentry",
-      version="0.1.0",
+      version="0.2.0",
       description="Singer.io tap for extracting Sentry project events and project issues.",
       author="Stitch",
       url="http://singer.io",
